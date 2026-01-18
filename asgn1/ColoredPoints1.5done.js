7cb88d5d88904da5c28478d041e06968393fcd0d
DanielR007 <dmrothma@ucsc.edu>
2026-01-17 19:07:28 -0800

    Part 6 done

--- asgn1/ColoredPoints1.5done.js
+++ asgn1/ColoredPoints1.5done.js
@@ -108,33 +108,23 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
- 
-var g_shapesList = [];
-
-// var g_points = [];  // The array for the position of a mouse press
-// var g_colors = [];  // The array to store the color of a point
-// var g_sizes =  [];   // The array to store the size of a point
+var g_points = [];  // The array for the position of a mouse press
+var g_colors = [];  // The array to store the color of a point
+var g_sizes =  [];   // The array to store the size of a point
 
 function click(ev) {
 
   // Extract the x and y coordinates of the mouse click event
   let [x,y] = convertCoordinatesEventToGL(ev);
   
-  // Create a store the new point
-  let point = new Point();
-  point.position=[x, y];
-  point.color=g_selectedColor.slice();
-  point.size=g_selectedSize;
-  g_shapesList.push(point);
-  
   // Store the coordinates to g_points array
-  //g_points.push([x, y]);
+  g_points.push([x, y]);
 
   // Store the color to g_colors array
-  //g_colors.push(g_selectedColor.slice());
+  g_colors.push(g_selectedColor.slice());
 
   // Store the size to g_sizes array
-  //g_sizes.push(g_selectedSize);
+  g_sizes.push(g_selectedSize);
 
 //  if (x >= 0.0 && y >= 0.0) {      // First quadrant
 //    g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
@@ -167,13 +157,20 @@ function renderAllShapes() {
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  //var len = g_points.lenth;
-  var len = g_shapesList.length;
-  
+  var len = g_points.length;
   for(var i = 0; i < len; i++) {
-    
-    g_shapesList[i].render();
+    var xy = g_points[i];
+    var rgba = g_colors[i];
+    var size = g_sizes[i];
 
+    // Pass the position of a point to a_Position variable
+    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+    // Pass the color of a point to u_FragColor variable
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    // Pass the size of a point to u_Size variable
+    gl.uniform1f(u_Size, size);
+    // Draw
+    gl.drawArrays(gl.POINTS, 0, 1);
   }
 
 }
--- asgn1/ColoredPointsProblem.js
+++ asgn1/ColoredPointsProblem.js
@@ -108,12 +108,42 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
- 
-var g_shapesList = [];
+class Point {
+  constructor() {
+    this.type = 'point';
+    this.position = [0.0, 0.0, 0.0];
+    this.color = [1.0,1.0,1.0,1.0];
+    this.size = 5.0;
+  }
+  
+  render() {
+    var xy = this.position;
+    var rgba = this.color;
+    var size = this.size;
+//    var xy = g_shapesList[i].position;
+//    var rgba = g_shapesList[i].color;
+//    var size = g_shapesList[i].size;
 
-// var g_points = [];  // The array for the position of a mouse press
-// var g_colors = [];  // The array to store the color of a point
-// var g_sizes =  [];   // The array to store the size of a point
+    // Pass the position of a point to a_Position variable
+    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+    // Pass the color of a point to u_FragColor variable
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+    // Pass the size of a point to u_Size variable
+    gl.uniform1f(u_Size, size);
+
+    // Draw
+    gl.drawArrays(gl.POINTS, 0, 1);
+
+  } 
+
+}
+
+var g_shapesList = []; 
+
+//var g_points = [];  // The array for the position of a mouse press
+//var g_colors = [];  // The array to store the color of a point
+//var g_sizes =  [];   // The array to store the size of a point
 
 function click(ev) {
 
@@ -126,7 +156,7 @@ function click(ev) {
   point.color=g_selectedColor.slice();
   point.size=g_selectedSize;
   g_shapesList.push(point);
-  
+
   // Store the coordinates to g_points array
   //g_points.push([x, y]);
 
@@ -167,13 +197,14 @@ function renderAllShapes() {
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  //var len = g_points.lenth;
+  //var len = g_points.length;
   var len = g_shapesList.length;
-  
-  for(var i = 0; i < len; i++) {
-    
-    g_shapesList[i].render();
 
+  for(var i = 0; i < len; i++) {
+
+    g_shapesList.render();
+
+  
   }
 
 }
